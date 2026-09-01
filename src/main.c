304b45f163c5ac3ffedc2e08d6b5be7c681d9219
Atvik Sharma <sharmaatvik@gmail.com>
2026-09-01 14:36:49 +0000

    Update compiler to accept test files

--- src/main.c
+++ src/main.c
@@ -3,25 +3,43 @@
 
 #include "parser.h"
 
-int main(void) {
+#define MAX_SOURCE_SIZE 10000
 
-    const char *source =
-    "int a, b, x;"
-    "a = 5;"
-    "b = 10;"
-    "if (a < b && b > 0) {"
-    "x = a + b;"
-    "} else {"
-    "x = a - b;"
-    "}";
+int main(int argc, char *argv[]) {
+
+    FILE *file;
+    char source[MAX_SOURCE_SIZE];
+    size_t length;
 
     printf("========================================\n");
     printf("   TEAM 10 - MINI COMPILER\n");
     printf("   BCSE307L - COMPILER DESIGN\n");
     printf("========================================\n\n");
 
+    /* Check command-line argument */
+    if (argc != 2) {
+        printf("Usage: %s <test_file>\n", argv[0]);
+        printf("\nExample:\n");
+        printf("  %s tests/test_assignment.c\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    /* Open test file */
+    file = fopen(argv[1], "r");
+
+    if (file == NULL) {
+        printf("Error: Cannot open file '%s'\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    /* Read source program */
+    length = fread(source, 1, MAX_SOURCE_SIZE - 1, file);
+    source[length] = '\0';
+
+    fclose(file);
+
     printf("Input Program:\n");
-    printf("%s\n\n", source);
+    printf("%s\n", source);
 
     printf("Starting compilation...\n");
 
@@ -32,5 +50,5 @@ int main(void) {
 
     print_tac();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
